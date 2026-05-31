4477b58e608a9203c4712d45139d71055a0f80b6
arashsina444-hash <arashsina444@gmail.com>
2026-05-31 11:04:54 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+import os
+import requests
+import json
+from datetime import datetime
+
+print("بستر ایجنت اوراکل فوتبال جام جهانی ۲۰۲۶ با موفقیت راه‌اندازی شد!")
+print("آماده برای اتصال به منابع خبری و دیتابیس‌های ورزشی...")
